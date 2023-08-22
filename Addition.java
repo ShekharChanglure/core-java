131d7eedc50246b81f3d0eca84a6ba2069728948
SHEKHAR-XWORKZ <shekharxworkz@gmail.com>
2023-08-22 11:25:02 +0530

    massage

---
+++ Addition.java
@@ -0,0 +1,45 @@
+class  Addition{
+
+public static void main(String args[]){
+	
+	/*int total = 45+57 ;
+	int total1= 55+54 ;
+	int total2= 49+575 ;
+	int total3= 457+57 ;
+	int total4 = 405+57 ;
+	int total5= 55+57 ;
+	int total6 = 05+57 ;
+	int total7 = 46+17 ;
+	int total8 = 45+53 ;
+	int total9 = 35+56 ;
+	
+	System.out.println("sum of two  number is  " + total);
+	System.out.println("sum of two  number is  " + total1);
+	System.out.println("sum of two number is   " + total2);
+	System.out.println("sum of two  number is  " + total3);
+	System.out.println("sum of two  number is  " + total4);
+	System.out.println("sum of two  number is  " + total5);
+	System.out.println("sum of two  number is  " + total6);
+    System.out.println("sum of two  number is  " + total7);
+    System.out.println("sum of two  number is  " + total8);
+	System.out.println("sum of two  number is  " + total9);*/
+	
+	
+	add(45 , 67);
+	add(1 , 64 );
+	add(75 , 63849474 );
+	add(7 , 656 );
+	add(65 , 07);
+	add(784 ,55 );
+	add(3 , 18 );
+	add(785 , 98 );
+	add(46 , 0 );
+	add(87 , 6 );
+                                                   }
+													  
+					//method - add
+		   public static void add(int a , int b)
+		             {
+			   System.out.println(a+b);
+	                  }
+}
